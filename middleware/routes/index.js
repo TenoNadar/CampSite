43c4e2ec24a6f60ba2544f52f4fde27c4ea7dc4a
TenoNadar <nadarteno3@gmail.com>
2018-06-23 15:28:24 +0530

    index for authentication

--- middleware/routes/index.js
+++ middleware/routes/index.js
@@ -10,3 +10,27 @@ var User = require('../models/user');
 router.get('/', function(req, res) {
     res.render('landing');
   });
+
+  // Auth routes
+
+router.get('/register', function(req, res) {
+    res.render('register');
+  });
+  
+  router.post('/register', function(req, res) {
+    var newUser = new User({
+      username: req.body.username
+    });
+    User.register(newUser, req.body.password, function(err, user) {
+      if (err) {
+        req.flash('error', err.message);
+        res.redirect('register');
+      } else {
+        passport.authenticate('local')(req, res, function() {
+          req.flash('success', wecomeMessage + user.username);
+          res.redirect('/campgrounds');
+        });
+      }
+    });
+  });
+  
