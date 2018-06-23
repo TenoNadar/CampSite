b57140fed9ecab22b29b1d66f00f8b446049dd30
TenoNadar <nadarteno3@gmail.com>
2018-06-23 14:54:12 +0530

    First middleware done

--- middleware/index.js
+++ middleware/index.js
@@ -3,3 +3,12 @@ var Campground = require('../models/campground'),
   errMessage = 'You can only make changes to a campground you added';
 
 var middlewareObj = {};
+
+middlewareObj.isLoggedIn = function(req, res, next) {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      req.flash('error', 'Please Log in First');
+      res.redirect('/login');
+    }
+  };
