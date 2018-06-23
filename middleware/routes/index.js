fb6728026bd2258dd5dfbbc231194affa6983bbc
TenoNadar <nadarteno3@gmail.com>
2018-06-23 15:15:29 +0530

    campground index  done

---
+++ middleware/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router({
+  mergeParams: true
+});
+
+var Campground = require('../models/campground'),
+  middleware = require('../middleware');
+
+// Campground INDEX
+
+router.get('/', function(req, res) {
+  Campground.find({}, function(err, allCampgrounds) {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.render('campgrounds/index', {
+        campgrounds: allCampgrounds
+      });
+    }
+  });
+});
