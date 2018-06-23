10d4913af67522da3503055635f7a09c1efcc3b0
TenoNadar <nadarteno3@gmail.com>
2018-06-23 15:23:20 +0530

    model for new

--- middleware/routes/comments.js
+++ middleware/routes/comments.js
@@ -6,3 +6,17 @@ var router = express.Router({
 var Campground = require('../models/campground'),
   Comment = require('../models/comment'),
   middleware = require('../middleware');
+// Campground Comment NEW
+
+router.get('/new', middleware.isLoggedIn, function(req, res) {
+    Campground.findById(req.params.campground_id, function(err, foundCampground) {
+      if (err) {
+        req.flash('error', 'Campground was not found');
+        res.redirect('back');
+      } else {
+        res.render('comments/new', {
+          campground: foundCampground
+        });
+      }
+    });
+  });
