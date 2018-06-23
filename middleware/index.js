c1d22e8b214549eb3ed911e4fae274bb0d666708
TenoNadar <nadarteno3@gmail.com>
2018-06-23 14:53:44 +0530

    First middleware

---
+++ middleware/index.js
@@ -0,0 +1,5 @@
+var Campground = require('../models/campground'),
+  Comment = require('../models/comment'),
+  errMessage = 'You can only make changes to a campground you added';
+
+var middlewareObj = {};
