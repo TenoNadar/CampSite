7f00a4bb6ef500634fdaff0c4962c0226ae421f7
TenoNadar <nadarteno3@gmail.com>
2018-06-23 15:17:24 +0530

    campground created

--- middleware/routes/index.js
+++ middleware/routes/index.js
@@ -25,3 +25,29 @@ router.get('/', function(req, res) {
 router.get('/new', middleware.isLoggedIn, function(req, res) {
     res.render('campgrounds/new');
   });
+  // Campground CREATE
+
+router.post('/', middleware.isLoggedIn, function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var author = {
+      id: req.user._id,
+      username: req.user.username
+    };
+    var newCampground = {
+      name: name,
+      image: image,
+      description: desc,
+      author: author
+    };
+    Campground.create(newCampground, function(err, newlyCreated) {
+      if (err) {
+        req.flash('error', 'Campground could not be created');
+        res.redirect('back');
+      } else {
+        req.flash('success', 'Campground was added');
+        res.redirect('/campgrounds/' + newlyCreated._id);
+      }
+    });
+  });
