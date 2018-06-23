90e36abcb9381dd21e8b9a211e2321b76a6425bd
TenoNadar <nadarteno3@gmail.com>
2018-06-23 14:54:45 +0530

    First to check

--- middleware/index.js
+++ middleware/index.js
@@ -12,3 +12,22 @@ middlewareObj.isLoggedIn = function(req, res, next) {
       res.redirect('/login');
     }
   };
+
+  middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+    if (req.isAuthenticated()) {
+      Campground.findById(req.params.campground_id, function(err, foundCampgrnd) {
+        if (err) {
+          req.flash('error', 'Campground was not found');
+          res.redirect('back');
+        } else if (foundCampgrnd.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          req.flash('error', errMessage);
+          res.redirect('back');
+        }
+      });
+    } else {
+      req.flash('error', 'Please Log in First');
+      res.redirect('/login');
+    }
+  };
